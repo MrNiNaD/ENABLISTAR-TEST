796cc25314fc6f4f59f0b78568fa435a57f28d54
MrNiNaD <ninadparkar273@gmail.com>
2024-06-22 18:15:57 +0530

    feat: mark up added for topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { updateUserState } from './redux/slice/user'
+import { BrowserRouter } from "react-router-dom"
+import TopBar from "./component/TopBar"
 
 function App() {
-  const test = useSelector((state) => state?.user);
-  const [count, setCount] = useState(0)
-
-  const dispatch = useDispatch();
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => {
-          setCount((count) => count + 1);
-          dispatch(updateUserState({ data: [1] }))
-        }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TopBar />
+      <BrowserRouter>
+        
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/component/TopBar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import HdfcLogo from '../assets/hdfc_logo.gif'
+
+const TopBar = () => {
+  const data = ["Smart Buy", "Personalize User ID", "Insta Alert", "SMS Banking Registation", "Contact Us"];
+  const navLinks = ["Accounts", "Fund Transfer", "BillPay & Recharge", "Cards", "Demat", "Mutual Funds", "Insurance", "Loans", "Offer"];
+
+  return (
+    <header>
+      <div className='section-1'>
+        <ul>
+          {data.map((eachData, index) => (
+            <li key={index}>
+              {eachData}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className='section-2'>
+        <h1>
+          <span className='net-banking'>
+            <span className='net'>Net</span>
+            <span className='banking'>Banking</span>
+          </span>
+          <img src={HdfcLogo} />
+        </h1>
+
+        <div className='right-section'>
+          <div className='right-section-part-1'>
+            <div className='left'>
+              <span className='welcome-text'>WELCOME, NINAD</span>
+              <span className='login-time'>Last Login: Jul 09 2020 3:39 PM IST</span>
+            </div>
+            <div className='right'>
+              <ul>
+                <li>Change Password</li>
+                <li>Update Contact Details</li>
+                <li>
+                  <button>Logout</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='right-section-part-2'>
+            <ul>
+              {navLinks?.map((navLink, index) => {
+                return <li className={navLink === "Fund Transfer" ? "nav-link-active" : ""} key={index}>{navLink}</li>
+              })}
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default TopBar
