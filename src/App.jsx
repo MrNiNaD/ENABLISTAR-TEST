54d7853a9261c11a38c819322f085f309dab7eab
MrNiNaD <ninadparkar273@gmail.com>
2024-06-22 16:41:45 +0530

    feat: redux and redux persist setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './redux/slice/counter'
 
 function App() {
+  const test = useSelector((state) => state.counter);
+  console.log("test", test);
   const [count, setCount] = useState(0)
+
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -18,7 +24,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+          setCount((count) => count + 1);
+          dispatch(increment())
+        }}>
           count is {count}
         </button>
         <p>
---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import counterReducer from './slice/counter';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['counter'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+const persistor = persistStore(store)
+
+export { store, persistor }
